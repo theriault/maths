3830d5d397abdb6894de19f4d1987fcde6b604b7
Dustin Thériault <dustintheriault@gmail.com>
2022-05-25 22:52:55 -0600

    Update aliquot_sum.go
    should return 0 for 1

--- numbertheory/aliquot_sum.go
+++ numbertheory/aliquot_sum.go
@@ -8,8 +8,5 @@ import "github.com/theriault/maths"
 //
 // https://oeis.org/A001065
 func AliquotSum[A maths.Integer](n A) uint64 {
-	if n == 1 {
-		return 1
-	}
 	return SumOfDivisors(n) - uint64(n)
 }
